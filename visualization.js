601a2816e74bc6a3a7f2b610dd1da3ef9a596e55
Akash Shetye <shetyeakash@gmail.com>
2017-01-10 13:32:42 -0600

    Have bare bones line chart from data

--- visualization.js
+++ visualization.js
@@ -34,16 +34,16 @@ function buildVisualization(){
 	var parseTime = d3.timeParse("%m/%d/%Y");
 	//line function for currency in circulation
 	var lineCurrency = d3.line()
-					.x(function(d){ return xScale(d.currency); })
-					.y(function(d){ return yScale(d.date); });
+					.x(function(d){ return xScale(d.date); })
+					.y(function(d){ return yScale(d.currency); });
 	//line function for non-govt deposits
 	var lineNonGovtDeposits = d3.line()
-							.x(function(d){ return xScale(d.nonGovtDeposits); })
-							.y(function(d){ return yScaled(d.date); });
+							.x(function(d){ console.log(d.date, xScale(d.date));return xScale(d.date); })
+							.y(function(d){ return yScale(d.nonGovtDeposits); });
 	//line function for government deposits
 	var lineGovtDeposits = d3.line()
-							.x(function(d){ return xScale(d.govtDeposits); })
-							.y(function(d){ return yScaled(d.date); });
+							.x(function(d){ return xScale(d.date); })
+							.y(function(d){ return yScale(d.govtDeposits); });
 	d3.tsv("RBI_Stats.tsv", function(d){
 		d.date = parseTime( d["MM/DD/YYYY"] );
 		d.currency = +d["Notes in Circulation"];
@@ -54,9 +54,43 @@ function buildVisualization(){
 	function(error, data){
 		if(error) throw error;
 		//determine the domain for currency, nonGovtDeposits and govtDeposits
-		console.log(data);
-		currencyExtent = d3.extent(data, function(d){ return d.currency; });
-		nonGovtDepositsExtent = d3.extent(data, function(d){ return r.nonGovtDeposits; });
-		govtDepositsExtent = d3.extent(data, function);
+		var cumulativeExtent = [];
+		cumulativeExtent = cumulativeExtent.concat(d3.extent(data, function(d){ return d.currency; }));
+		cumulativeExtent = cumulativeExtent.concat(d3.extent(data, function(d){ return d.nonGovtDeposits; }));
+		cumulativeExtent = cumulativeExtent.concat(d3.extent(data, function(d){ return d.govtDeposits; }));
+		cumulativeExtent = d3.extent(cumulativeExtent);
+		dateExtent = d3.extent(data, function(d){ return d.date; });
+		xScale.domain(dateExtent);
+		//for govt deposits too low fixing looks
+		yScale.domain([0, cumulativeExtent[1]]);
+
+		g.append("g")
+			.attr("class", "axis axis--x")
+			.attr("transform", "translate(0," + height + ")")
+			.call(d3.axisBottom(xScale));
+
+		g.append("g")
+			.attr("class", "axis axis--y")
+			.call(d3.axisLeft(yScale))
+				.append("text")
+				.attr("fill", "#000")
+				.attr("transform", "rotate(-90)")
+				.attr("y", 6)
+				.attr("dy", "0.71em")
+				.style("text-anchor", "end")
+				.text("Price ($)");
+
+		g.append("path")
+			.datum(data)
+			.attr("class", "line")
+			.attr("d", lineGovtDeposits);
+		g.append("path")
+            .datum(data)
+            .attr("class", "line")
+            .attr("d", lineNonGovtDeposits);
+		g.append("path")
+            .datum(data)
+            .attr("class", "line")
+            .attr("d", lineCurrency);
 	});
 };
